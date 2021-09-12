23d3e84d5978d28d5b9fe6643786269cbc85393c
derekthecarrot <jpastvait@gmail.com>
2021-09-12 14:30:17 -0400

    working with json data and understanding how to implement and update the html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
-var APIKey = "45d16616d8a2ece41cc37e7a1088d048"
-
-var city = "";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-
+var city = "Raleigh"
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=45d16616d8a2ece41cc37e7a1088d048" + "&units=imperial";
 
 fetch(queryURL)
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    document.querySelector("#CityText").innerText = city;
+    document.querySelector("#Temp").innerText = "Temperature: " + data.main.temp
+    document.querySelector("#Wind").innerText = "Wind: " + data.wind.speed
+    document.querySelector("#Humidity").innerText = "Humidity: " + data.main.humidity
+    document.querySelector("#UV-Index").innerText = "UV-Index: "
+  });
