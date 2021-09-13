39c12a3707ba3aa76f56599846c6c33bed9bdc68
derekthecarrot <jpastvait@gmail.com>
2021-09-13 00:42:07 -0400

    querying through with ajax for UVIndex, creating and appending UVIndex info

--- assets/script.js
+++ assets/script.js
@@ -70,3 +70,73 @@ function populateCityWeather(city, citySearch) {
 
       latitude = weather.coord.lat;
       longitude = weather.coord.lon;
+
+      const queryUVIndex =
+        "https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=45d16616d8a2ece41cc37e7a1088d048&q=" +
+        "&lat=" +
+        latitude +
+        "&lon=" +
+        longitude;
+
+      $.ajax({
+        url: queryUVIndex,
+        method: "GET"
+        // Store all of the retrieved data inside of an object called "uvIndex"
+      }).then(function(uvIndex) {
+        console.log(uvIndex);
+
+        var uvIndexDisplay = $("<button>");
+        uvIndexDisplay.addClass("btn btn-danger");
+
+        $("#current-uv").text("UV Index: ");
+        $("#current-uv").append(uvIndexDisplay.text(uvIndex[0].value));
+        console.log(uvIndex[0].value);
+
+        $.ajax({
+          url: queryForecast,
+          method: "GET"
+          // Store all of the retrieved data inside of an object called "forecast"
+        }).then(function(forecast) {
+          console.log(queryForecast);
+
+          console.log(forecast);
+          // Loop through the forecast list array and display a single forecast entry/time (5th entry of each day which is close to the highest temp/time of the day) from each of the 5 days
+          for (var i = 6; i < forecast.list.length; i += 8) {
+            // 6, 14, 22, 30, 38
+            var forecastDate = $("<h5>");
+
+            var forecastPosition = (i + 2) / 8;
+
+            console.log("#forecast-date" + forecastPosition);
+
+            $("#forecast-date" + forecastPosition).empty();
+            $("#forecast-date" + forecastPosition).append(
+              forecastDate.text(nowMoment.add(1, "days").format("M/D/YYYY"))
+            );
+
+            const forecastIcon = $("<img>");
+            forecastIcon.attr(
+              "src","https://openweathermap.org/img/wn/" + forecast.list[i].weather[0].icon + ".png"
+            );
+
+            $("#forecast-icon" + forecastPosition).empty();
+            $("#forecast-icon" + forecastPosition).append(forecastIcon);
+
+            console.log(forecast.list[i].weather[0].icon);
+
+            $("#forecast-temp" + forecastPosition).text(
+              "Temp: " + forecast.list[i].main.temp + " F"
+            );
+            $("#forecast-humidity" + forecastPosition).text(
+              "Humidity: " + forecast.list[i].main.humidity + "%"
+            );
+
+            $(".forecast").attr(
+              "style", "background-color:rgba(71, 156, 212, 0.637); color:white"
+            );
+          }
+        });
+      });
+    });
+}
+
