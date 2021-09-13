e898d0f37562ebc76d77d7499bbd1115fc0f974a
derekthecarrot <jpastvait@gmail.com>
2021-09-13 00:41:20 -0400

    querying through with ajax, creating and appending text to display proper weather info

--- assets/script.js
+++ assets/script.js
@@ -18,3 +18,55 @@ function createList(citySearch) {
   }
 }
 
+function populateCityWeather(city, citySearch) {
+  createList(citySearch);
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=45d16616d8a2ece41cc37e7a1088d048&q=" +
+    city;
+
+  var queryForecast =
+  "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=45d16616d8a2ece41cc37e7a1088d048&q=" +
+    city;
+
+  var latitude;
+
+  var longitude;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // Store all of the retrieved data inside of an object called "weather"
+    .then(function(weather) {
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(weather);
+
+      var nowMoment = moment();
+
+      const displayMoment = $("<h3>");
+      $("#city-name").empty();
+      $("#city-name").append(
+        displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
+      );
+
+      const cityName = $("<h3>").text(weather.name);
+      $("#city-name").prepend(cityName);
+
+      const weatherIcon = $("<img>");
+      weatherIcon.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png"
+      );
+      $("#iconweather").empty();
+      $("#iconweather").append(weatherIcon);
+
+      $("#current-temp").text("Temperature: " + weather.main.temp + " °F");
+      $("#current-humidity").text("Humidity: " + weather.main.humidity + "%");
+      $("#current-wind").text("Wind Speed: " + weather.wind.speed + " MPH");
+
+      latitude = weather.coord.lat;
+      longitude = weather.coord.lon;
